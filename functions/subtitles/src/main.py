f59ca2d51afad9bc6145cdca427da3620dc34d0b
mr-destructive <gormeet711@gmail.com>
2024-08-17 17:51:17 +0530

    add test appwrite function

--- functions/subtitles/src/main.py
+++ functions/subtitles/src/main.py
@@ -1,7 +1,5 @@
 import json
-import os
 
-from appwrite.client import Client
 from youtube_transcript_api import YouTubeTranscriptApi
 
 def subtitle_extract(url):
@@ -16,37 +14,15 @@ def subtitle_extract(url):
     print(e)
     return ""
 
-# This is your Appwrite function
-# It's executed each time we get a request
 def main(context):
-    # Why not try the Appwrite SDK?
-    #
-    # client = (
-    #     Client()
-    #     .set_endpoint("https://cloud.appwrite.io/v1")
-    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-    #     .set_key(os.environ["APPWRITE_API_KEY"])
-    # )
-
-    # You can log messages to the console
     context.log("Hello, Logs!")
 
-    # If something goes wrong, log an error
-    context.error("Hello, Errors!")
-
-    # The `ctx.req` object contains the request data
-    if context.req.method == "GET":
-        # Send a response with the res object helpers
-        # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World!")
-
     req = context.req
-    body = json.dumps(context.req.body)
+    body = json.dumps(req.body)
     context.log(body)
     subtitles = subtitle_extract(body)
     context.log(subtitles)
 
-    # `ctx.res.json()` is a handy helper for sending JSON
     return context.res.json(
         {
             "subtitles": subtitles
