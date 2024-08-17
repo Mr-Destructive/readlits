a011494cb6356d322b208b593ce8fb3b8b32279c
mr-destructive <gormeet711@gmail.com>
2024-08-17 18:09:10 +0530

    add test appwrite function

--- functions/subtitles/src/main.py
+++ functions/subtitles/src/main.py
@@ -2,17 +2,26 @@ import json
 
 from youtube_transcript_api import YouTubeTranscriptApi
 
+
 def subtitle_extract(url):
-  url = url.split('/watch?v=')[1]
-  subtitle_text = ""
-  subtitles = YouTubeTranscriptApi.get_transcript(url)
-  try:
-    for subtitle in subtitles:
-        subtitle_text += " " + subtitle.get("text", "")
-    return subtitle_text
-  except Exception as e:
-    print(e)
-    return ""
+    url = url.split("/watch?v=")
+    if url and len(url) > 1:
+        url = url[1]
+    subtitle_text = ""
+    subtitles = YouTubeTranscriptApi.get_transcript(
+        url,
+        languages=[
+            "en",
+        ],
+    )
+    try:
+        for subtitle in subtitles:
+            subtitle_text += " " + subtitle.get("text", "")
+        return subtitle_text
+    except Exception as e:
+        print(e)
+        return ""
+
 
 def main(context):
     context.log("Hello, Logs!")
@@ -23,8 +32,8 @@ def main(context):
     subtitles = subtitle_extract(body)
     context.log(subtitles)
 
-    return context.res.json(
-        {
-            "subtitles": subtitles
-        }
-    )
+    return context.res.json({"subtitles": subtitles})
+
+
+# r = subtitle_extract("https://www.youtube.com/watch?v=Ju33zD5A8Hg")
+# print(r)
